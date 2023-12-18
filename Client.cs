e2d757f6e36d0430029c32f7355e8ecd948aa277
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-18 09:41:53 +0100

    ADD : prenom+nom+age + requete

---
+++ Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLinq
+{
+    public class Client
+    {
+        public string _prenom; 
+        public string _nom; 
+        public int _age; 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! \nJe test le LINQ !!!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> clients = new List<Client>
+            {
+                new Client { _prenom = "Patrice1", _nom = "HANNE1", _age = 10 },
+                new Client { _prenom = "Patrice2", _nom = "HANNE2", _age = 20 },
+                new Client { _prenom = "Patrice3", _nom = "HANNE3", _age = 30 },
+                new Client { _prenom = "Patrice4", _nom = "HANNE4", _age = 40 },
+                new Client { _prenom = "Patrice5", _nom = "HANNE5", _age = 50 },
+            };
+
+            IEnumerable<Client> requete = 
+                from cli in clients where cli._age > 25 select cli;
+
+            foreach(Client c in requete) 
+            {
+                Console.WriteLine(c._prenom);
+            }
+        }
+    }
+}
